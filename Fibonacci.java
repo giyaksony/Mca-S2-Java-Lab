bed566c271e0a5dd53a5d82ba7b1da6e2a9d2bdf
giyaksony <143182287+giyaksony@users.noreply.github.com>
2025-02-12 23:18:20 +0700

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void fibo(int n) {
+        if (n <= 0) {
+            System.out.println("Please enter a positive number.");
+            return;
+        }
+
+        int a = 0, b = 1; 
+        System.out.print("Fibonacci series: " + a); 
+        
+        if (n > 1) {
+            System.out.print(", " + b); 
+        }
+
+        for (int i = 2; i < n; i++) { 
+            int c = a + b;
+            System.out.print(", " + c);
+            a = b; 
+            b = c;
+        }
+        System.out.println(); 
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of terms: ");
+        int n = sc.nextInt();
+        fibo(n);
+        sc.close();
+    }
+}
+
+// Output
+// Enter the number of terms: 4
+// Fibonacci series: 0, 1, 1, 2
